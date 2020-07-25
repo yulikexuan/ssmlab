65c6bc78dd74adb0b5c6cc1244dc874c32db5b85
yul <yu.li@tecsys.com>
2020-07-25 13:33:47 -0400

    Add new Enums to define States and State Events. Closes #1

---
+++ src/main/java/com/yulikexuan/sfg/ssmlab/domain/PaymentEvent.java
@@ -0,0 +1,15 @@
+//: com.yulikexuan.sfg.ssmlab.domain.PaymentEvent.java
+
+
+package com.yulikexuan.sfg.ssmlab.domain;
+
+public enum PaymentEvent {
+
+    PRE_AUTHORIZE,
+    PRE_AUTH_APPROVED,
+    PRE_AUTH_DECLINED,
+    AUTHORIZE,
+    AUTH_APPROVED,
+    AUTH_DECLINED,
+
+}///:~
---
+++ src/main/java/com/yulikexuan/sfg/ssmlab/domain/PaymentState.java
@@ -0,0 +1,10 @@
+//: com.yulikexuan.sfg.ssmlab.domain.PaymentState.java
+
+
+package com.yulikexuan.sfg.ssmlab.domain;
+
+public enum PaymentState {
+
+    NEW, PRE_AUTH, PRE_AUTH_ERROR, AUTH, AUTH_ERROR,
+
+}///:~
