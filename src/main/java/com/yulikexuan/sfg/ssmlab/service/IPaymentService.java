7fe6eb18d5286feb1f04dc01152f6e6988e12270
yul <yu.li@tecsys.com>
2020-07-28 09:56:18 -0400

    Create and save a new payment using payment service. Closes #6

--- src/main/java/com/yulikexuan/sfg/ssmlab/service/IPaymentService.java
+++ src/main/java/com/yulikexuan/sfg/ssmlab/service/IPaymentService.java
@@ -3,6 +3,23 @@
 
 package com.yulikexuan.sfg.ssmlab.service;
 
+
+import com.yulikexuan.sfg.ssmlab.domain.Payment;
+import com.yulikexuan.sfg.ssmlab.domain.PaymentEvent;
+import com.yulikexuan.sfg.ssmlab.domain.PaymentState;
+import org.springframework.statemachine.StateMachine;
+
+import java.util.UUID;
+
+
 public interface IPaymentService {
+
+    Payment newPayment(Payment payment);
+
+    StateMachine<PaymentState, PaymentEvent> preAuth(UUID paymentId);
+
+    StateMachine<PaymentState, PaymentEvent> authorizePayment(UUID paymentId);
+
+    StateMachine<PaymentState, PaymentEvent> declineAuth(UUID paymentId);
 
 }///:~
--- src/main/java/com/yulikexuan/sfg/ssmlab/service/PaymentService.java
+++ src/main/java/com/yulikexuan/sfg/ssmlab/service/PaymentService.java
@@ -4,15 +4,46 @@
 package com.yulikexuan.sfg.ssmlab.service;
 
 
+import com.yulikexuan.sfg.ssmlab.domain.Payment;
+import com.yulikexuan.sfg.ssmlab.domain.PaymentEvent;
+import com.yulikexuan.sfg.ssmlab.domain.PaymentState;
 import com.yulikexuan.sfg.ssmlab.repository.IPaymentRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+
+import java.util.UUID;
 
 
-@Service
+
+@Slf4j
 @RequiredArgsConstructor
-public class PaymentService implements IPaymentService {
+final class PaymentService implements IPaymentService {
 
     private final IPaymentRepository paymentRepository;
+
+    private final StateMachineFactory<PaymentState, PaymentEvent> stateMachineFactory;
+
+    @Override
+    public Payment newPayment(Payment payment) {
+        payment.setState(PaymentState.NEW);
+        return this.paymentRepository.save(payment);
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> preAuth(UUID paymentId) {
+        return null;
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> authorizePayment(UUID paymentId) {
+        return null;
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> declineAuth(UUID paymentId) {
+        return null;
+    }
 
 }///:~
