b0a6f97220bf943aff58abd33b2940f87f6db85e
yul <yu.li@tecsys.com>
2020-07-31 07:33:38 -0400

    Rehydrate a state machine with a Payment. Closes #7

--- src/main/java/com/yulikexuan/sfg/ssmlab/service/PaymentService.java
+++ src/main/java/com/yulikexuan/sfg/ssmlab/service/PaymentService.java
@@ -8,16 +8,22 @@ import com.yulikexuan.sfg.ssmlab.domain.Payment;
 import com.yulikexuan.sfg.ssmlab.domain.PaymentEvent;
 import com.yulikexuan.sfg.ssmlab.domain.PaymentState;
 import com.yulikexuan.sfg.ssmlab.repository.IPaymentRepository;
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.StateMachineContext;
+import org.springframework.statemachine.access.StateMachineAccessor;
+import org.springframework.statemachine.access.StateMachineFunction;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 
-
 @Slf4j
+@Service
 @RequiredArgsConstructor
 final class PaymentService implements IPaymentService {
 
@@ -44,6 +50,42 @@ final class PaymentService implements IPaymentService {
     @Override
     public StateMachine<PaymentState, PaymentEvent> declineAuth(UUID paymentId) {
         return null;
+    }
+
+    private StateMachine<PaymentState, PaymentEvent> buildAndRehydrate(UUID paymentId) {
+
+        Payment payment = this.paymentRepository.getOne(paymentId);
+
+        StateMachine<PaymentState, PaymentEvent> stateMachine =
+                this.stateMachineFactory.getStateMachine(payment.getId());
+
+        stateMachine.stop();
+
+        /*
+         * StateMachine provides an APIs for generic finite state machine
+         * needed for basic operations like working with states, events and a
+         * lifecycle
+         */
+        StateMachineAccessor<PaymentState, PaymentEvent> stateMachineAccessor =
+                stateMachine.getStateMachineAccessor();
+
+        /*
+         * Execute given StateMachineFunction with all recursive regions
+         *
+         * A region is an orthogonal part of either a composite state or a
+         * state machine
+         *   - It contains states and transition
+         *
+         * StateMachineContext represents a current state of a state machine
+         */
+        stateMachineAccessor.doWithAllRegions(
+                stateMachineAccess -> stateMachineAccess.resetStateMachine(
+                        new DefaultStateMachineContext<>(payment.getState(),
+                                null, null, null)));
+
+        stateMachine.start();
+
+        return stateMachine;
     }
 
 }///:~
