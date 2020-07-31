09e37a64fbf2ec320f6f86decae654966159950d
yul <yu.li@tecsys.com>
2020-07-31 08:09:19 -0400

    Sending Payment Event to State Machine. Closes #8

--- src/main/java/com/yulikexuan/sfg/ssmlab/service/PaymentService.java
+++ src/main/java/com/yulikexuan/sfg/ssmlab/service/PaymentService.java
@@ -8,13 +8,12 @@ import com.yulikexuan.sfg.ssmlab.domain.Payment;
 import com.yulikexuan.sfg.ssmlab.domain.PaymentEvent;
 import com.yulikexuan.sfg.ssmlab.domain.PaymentState;
 import com.yulikexuan.sfg.ssmlab.repository.IPaymentRepository;
-import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.StateMachine;
-import org.springframework.statemachine.StateMachineContext;
 import org.springframework.statemachine.access.StateMachineAccessor;
-import org.springframework.statemachine.access.StateMachineFunction;
 import org.springframework.statemachine.config.StateMachineFactory;
 import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
@@ -26,6 +25,8 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 final class PaymentService implements IPaymentService {
+
+    static final String PAYMENT_ID_MSG_HEADER = "payment_ic";
 
     private final IPaymentRepository paymentRepository;
 
@@ -39,20 +40,42 @@ final class PaymentService implements IPaymentService {
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> preAuth(UUID paymentId) {
+
+        StateMachine<PaymentState, PaymentEvent> stateMachine =
+                this.buildStateMachineWithPayment(paymentId);
+
+        this.sendPaymentEventToStateMachine(paymentId, stateMachine,
+                PaymentEvent.PRE_AUTHORIZE);
+
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> authorizePayment(UUID paymentId) {
+
+        StateMachine<PaymentState, PaymentEvent> stateMachine =
+                this.buildStateMachineWithPayment(paymentId);
+
+        this.sendPaymentEventToStateMachine(paymentId, stateMachine,
+                PaymentEvent.AUTH_APPROVED);
+
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> declineAuth(UUID paymentId) {
+
+        StateMachine<PaymentState, PaymentEvent> stateMachine =
+                this.buildStateMachineWithPayment(paymentId);
+
+        this.sendPaymentEventToStateMachine(paymentId, stateMachine,
+                PaymentEvent.AUTH_DECLINED);
+
         return null;
     }
 
-    private StateMachine<PaymentState, PaymentEvent> buildAndRehydrate(UUID paymentId) {
+    private StateMachine<PaymentState, PaymentEvent> buildStateMachineWithPayment(
+            UUID paymentId) {
 
         Payment payment = this.paymentRepository.getOne(paymentId);
 
@@ -86,6 +109,18 @@ final class PaymentService implements IPaymentService {
         stateMachine.start();
 
         return stateMachine;
+    }
+
+    private void sendPaymentEventToStateMachine(
+            UUID paymentId,
+            StateMachine<PaymentState, PaymentEvent> stateMachine,
+            PaymentEvent paymentEvent) {
+
+        Message paymentMessage = MessageBuilder.withPayload(paymentEvent)
+                .setHeader(PAYMENT_ID_MSG_HEADER, paymentId)
+                .build();
+
+        stateMachine.sendEvent(paymentMessage);
     }
 
 }///:~
